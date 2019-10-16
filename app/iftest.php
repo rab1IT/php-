ff8a7c67b91b78162a2f3a331065372303b36887
user <user@mako.local>
2019-10-16 11:30:38 +0900

    練習用

---
+++ app/iftest.php
@@ -0,0 +1,11 @@
+<?php 
+$language = 7;
+if($language === 1){
+    echo 'こんにちは';
+}elseif($language === 2){
+    echo 'Hello';
+}elseif($language === 3){
+    echo 'Bonjour';
+}else{
+    echo '入力した数値が違います';
+}
---
+++ app/time.php
@@ -0,0 +1,7 @@
+<?php
+$time = data ('G');
+if($time <12){
+    echo '今午前です';
+}elseif($time >= 12){
+    echo '午後です';
+}
