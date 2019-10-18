244d8c771269e0ccf1fec51690327cdb68be4f7b
“rab1IT” <vf25g.messia@gmail.com>
2019-10-18 19:15:40 +0900

    練習

--- app/condition1.php
+++ app/condition1.php
@@ -2,9 +2,9 @@
 $score = 82;
 if($score >= 0 && $score < 60){
     echo '平均点以下です';
-}elseif($score >= 60 && $score <80){
+}elseif($score >= 60 && $score < 80){
     echo '平均点を超えています';
-}elseif($score >= 80 && $score <100){
+}elseif($score >= 80 && $score < 100){
     echo '優秀な点数です';
 }elseif($score === 100){
     echo '満点です';
